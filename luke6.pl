9e9688cd7f57f900b766270a22ffa18443ba8956
Martin Bore <martin.bore@usit.uio.no>
2014-12-08 01:19:29 +0100

    la til kjøreeksempel

--- luke6.pl
+++ luke6.pl
@@ -2,12 +2,15 @@
 
 use strict; use warnings;
 
- my %hash = ();
+my %hash = ();
 
 my $number = 8000;
 
 for (my $i=0; $i <= $number; $i++) { for (my $j=0; $j <= $number; $j++) {
 
-    my $result = $i*$j; if ($result != 0) { $hash{ $result } = $result; } }}
+        my $result = $i*$j; if ($result != 0) { $hash{ $result } = $result; } }}
 
- print "size of hash:  " . keys( %hash ) . ".\n";
+print "size of hash:  " . keys( %hash ) . ".\n";
+
+# > perl luke6.pl
+# size of hash:  14509549.
